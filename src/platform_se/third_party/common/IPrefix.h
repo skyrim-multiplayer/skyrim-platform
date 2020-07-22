b74ab893ec5cae89e1a9b4fed5396bfbbbeba382
Leonid Pospelov <pospelovlm@yandex.ru>
2020-07-22 20:43:59 +0300

    .

--- src/platform_se/third_party/common/IPrefix.h
+++ src/platform_se/third_party/common/IPrefix.h
@@ -11,7 +11,7 @@
 #pragma warning(disable: 4018 4200 4244 4267 4305 4288 4312 4311)
 
 // winxp and above
-#define _WIN32_WINNT 0x0501
+#define _WIN32_WINNT	0x0501
 
 #include <cstdlib>
 #include <cstdio>
