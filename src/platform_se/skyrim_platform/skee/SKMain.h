d4831085875558402b944df8d759f320f2450e84
Leonid Pospelov <pospelovlm@yandex.ru>
2020-07-22 20:32:26 +0300

    .

--- src/platform_se/skyrim_platform/skee/SKMain.h
+++
@@ -1,8 +0,0 @@
-
-class SKSEInterface;
-class PluginInfo;
-
-namespace SKEE {
-bool SKSEPlugin_Query(const SKSEInterface* skse, PluginInfo* info);
-bool SKSEPlugin_Load(const SKSEInterface* skse);
-}
